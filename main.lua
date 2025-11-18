2e55caabd1c481ba583fa879683484e67e44aa3b
PJL <pauljohannlorenz@proton.me>
2025-11-18 17:34:01 +0100

    Add basic layout

--- main.lua
+++ main.lua
@@ -7,11 +7,16 @@ function love.load()
 
     -- Fonts
     titleFont = love.graphics.newFont(24)
+    smallFont = love.graphics.newFont(12)
 
     -- Colors
     colors = {
         background = {0.05, 0.05, 0.08},
-        green = {0.2, 0.9, 0.3}
+        green = {0.2, 0.9, 0.3},
+        panel = {0.1, 0.12, 0.15},
+        yellow = {0.9, 0.8, 0.2},
+        greenDim = {0.1, 0.4, 0.15},
+        white = {1, 1, 1}
     }
 end
 
@@ -24,4 +29,35 @@ function love.draw()
     love.graphics.setFont(titleFont)
     love.graphics.setColor(colors.green)
     love.graphics.printf("SIGNAL LOST", 0, 20, 800, "center")
+
+    -- Radio panel
+    love.graphics.setColor(colors.panel)
+    love.graphics.rectangle("fill", 50, 80, 700, 180)
+
+    -- Frequency display
+    love.graphics.setFont(titleFont)
+    love.graphics.setColor(colors.yellow)
+    love.graphics.printf(string.format("%.1f MHz", -1), 50, 100, 700, "center") -- TODO: Change Frequency
+
+    -- Frequency bar
+    local barX = 100
+    local barY = 150
+    local barWidth = 600
+    local barHeight = 30
+
+    love.graphics.setColor(colors.greenDim)
+    love.graphics.rectangle("fill", barX, barY, barWidth, barHeight)
+
+    -- Signal strength meter
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(colors.white)
+    love.graphics.print("SIGNAL STRENGTH", 100, 200)
+    
+    local meterWidth = 200
+    love.graphics.setColor(colors.greenDim)
+    love.graphics.rectangle("fill", 100, 220, meterWidth, 20)
+
+    -- Message display
+    love.graphics.setColor(colors.panel)
+    love.graphics.rectangle("fill", 50, 280, 700, 280)
 end
